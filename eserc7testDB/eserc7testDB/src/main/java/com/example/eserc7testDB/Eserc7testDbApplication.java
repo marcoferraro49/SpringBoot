78c46ae49b4677bfa1681b97cf7245df8e329188
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-05-14 14:38:32 +0200

    Esercizio 7
    Esercizio "Testiamo la base dati"

---
+++ eserc7testDB/eserc7testDB/src/main/java/com/example/eserc7testDB/Eserc7testDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.eserc7testDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eserc7testDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eserc7testDbApplication.class, args);
+	}
+
+}
---
+++ eserc7testDB/eserc7testDB/src/test/java/com/example/eserc7testDB/Eserc7testDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eserc7testDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eserc7testDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
