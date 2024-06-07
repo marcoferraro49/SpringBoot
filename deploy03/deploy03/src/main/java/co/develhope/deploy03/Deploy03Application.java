8b00457ef1800932b50a4fe3635ea549cbba0bf4
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-06-07 12:55:37 +0200

    Esercizio "Deploy - Environments Variables 01"

---
+++ deploy03/deploy03/src/main/java/co/develhope/deploy03/Deploy03Application.java
@@ -0,0 +1,13 @@
+package co.develhope.deploy03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy03Application.class, args);
+	}
+
+}
---
+++ deploy03/deploy03/src/test/java/co/develhope/deploy03/Deploy03ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.deploy03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
